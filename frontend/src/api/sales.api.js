2d099485775fe8a7e0133cd37c903beb962f99d9
Rohit-Yadav-Official <roadtocode76448@gmail.com>
2025-12-09 14:22:12 +0530

    deployed backend again with server cpu config changes

--- frontend/src/api/sales.api.js
+++ frontend/src/api/sales.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://root-production-88c0.up.railway.app/api",
+  baseURL: "https://root-production-d0ea.up.railway.app/api",
 });
 
 export const getSales = async (filters = {}) => {
